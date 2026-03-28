3d9993774a04322dd3ccdd02602057dc61cb240b
pain. <painingit@gmail.com>
2026-03-28 23:20:03 +0900

    fix: acceleration and force calculation

--- Assets/Pain/Physics/Core/PhysTransform.cs
+++ Assets/Pain/Physics/Core/PhysTransform.cs
@@ -6,32 +6,48 @@ namespace Pain.Physics.Core
     [Serializable]
     public struct PhysTransform
     {
+        public PhysVector3 force        { get; private set; }
         public PhysVector3 velocity     { get; private set; }
         public PhysVector3 acceleration { get; private set; }
 
         public float mass { get; private set; }
+        private float invMass;
 
         private Transform m_transform;
 
         public PhysTransform(float mass, Transform transform)
         {
+            force = PhysVector3.zero;
             velocity = PhysVector3.zero;
             acceleration = PhysVector3.zero;
             
             this.mass = mass;
+            invMass = 1 / mass;
             m_transform = transform;
         }
 
         public PhysTransform AddForce(PhysVector3 force)
         {
-            acceleration += force * mass;
+            this.force += force;
+            return this;
+        }
+
+        public PhysTransform AddAcceleration(PhysVector3 acceleration)
+        {
+            this.acceleration += acceleration;
             return this;
         }
 
         public void Step(float dt)
         {
+            acceleration += force * invMass;
+            
             velocity += acceleration * dt;
             m_transform.position += (Vector3)velocity * dt;
+            
+            // reset values to prevent object speeding overtime
+            force = PhysVector3.zero;
+            acceleration = PhysVector3.zero;
         }
     }
 }
